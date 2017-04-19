c5c1cb5f0079524373a3f0a4640cce66592901a2
LaTotty <latotty91@gmail.com>
2017-04-20 00:11:23 +0200

    feat(init): theme provider, styled

--- imports/ui/components/App.js
+++ imports/ui/components/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { LandingPage } from '/imports/ui/components/pages/LandingPage';
 import { NotFoundPage } from '/imports/ui/components/pages/NotFoundPage';
 
+import theme from '/imports/ui/components/themes/default';
+
 export const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
+  </ThemeProvider>
 );
 
 export default App;
+
